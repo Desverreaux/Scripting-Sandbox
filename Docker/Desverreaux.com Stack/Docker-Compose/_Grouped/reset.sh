adf742c214a8ac6e81fc821eda3054aba530ac46
root <root@ip-10-0-0-174.ec2.internal>
2019-01-05 19:51:16 +0000

    reorganizing

---
+++ Docker/Desverreaux.com Stack/Docker-Compose/_Grouped/reset.sh
@@ -0,0 +1,11 @@
+docker kill PhpMyAdmin
+docker kill ApacheServer
+docker kill MySQL
+docker kill Nginx
+docker kill grouped_Portainer_1
+docker rm PhpMyAdmin
+docker rm ApacheServer
+docker rm MySQL
+docker rm Nginx
+docker rm grouped_Portainer_1
+docker-compose up
